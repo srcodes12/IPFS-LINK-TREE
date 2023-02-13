a63701bc6d66d8d216eddcceac9d39306c2774c2
Stephen Rogers <stephenrogerszk@gmail.com>
2023-02-12 22:41:14 -0700

    Cleaned up

--- gfxscript.js
+++ gfxscript.js
@@ -12,12 +12,6 @@ images[7] = "/images/xuxspace2.png";
 images[8] = "/images/xuxgfx.png";
 
 
-
-
-
-
-
-
 function changeLeft () {
   document.slide.src = images[i];
     if ((i <= 8) && (i > 0)) {
@@ -39,8 +33,6 @@ function changeRight () {
       }
     }
 }
-
-
 
 function img0 () {
   document.slide.src = images[0];
@@ -71,41 +63,3 @@ function img7 () {
 function img8 () {
   document.slide.src = images[8];
 }
-1111111111111111111111111111111111111111111111111111111111111111111111111
-/*
-const boxa = document.querySelector('boxa');
-const box1 = document.querySelector('.boxstyle33');
-const box2 = document.querySelector('.boxstyle44');
-const box3 = document.querySelector('.boxstyle55');
-const box4 = document.querySelector('.boxstyle66');
-const box5 = document.querySelector('.boxstyle77');
-const box6 = document.querySelector('.boxstyle88');
-
-function changeLeft () {
-  const show1 = document.querySelector('#show');
-    if (show1.className.includes('33')){
-      box1.setAttribute('hidden', 'true');
-      box6.removeAttribute('hidden');
-      box1.removeAttribute('id');
-      box6.id = 'show';
-}
-}
-
-function changeRight () {
-  const show1 = document.querySelector('#show');
-    if (show1.className.includes('33')){
-      box1.setAttribute('hidden', 'true');
-      box2.removeAttribute('hidden');
-      box1.removeAttribute('id');
-      box2.id = 'show';
-}
-}
-
-/*
-function changeRight () {
-   if (currentIndex === 0){
-    currentIndex = currentIndex + 1;
-    document.getElementById('box-2').appendChild(boxa)
-   }
-}
-*/
