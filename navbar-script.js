294e3f1b020818c5935de34f218575333c43d371
Stephen <stephenrogerszk@gmail.com>
2023-05-26 19:46:11 -0600

    Deleted indexgfx.css and make navbar dynamic

---
+++ navbar-script.js
@@ -0,0 +1,7 @@
+// Fetch the content of the navbar.html file
+fetch('navbar.html')
+  .then(response => response.text())
+  .then(html => {
+    // Insert the navbar content into the placeholder div
+    document.getElementById('navbar-placeholder').innerHTML = html;
+  });
